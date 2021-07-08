8779a1d04a994447b4502f845d36bfa490910427
vitaliikn <vitalja.dn@gmail.com>
2021-07-08 17:44:07 +0300

    add HW9 and changed HW7

---
+++ HW9-dom/main.js
@@ -0,0 +1,30 @@
+const wrapper = document.querySelector('.wrapper')
+
+const getRandomColor = () => {
+    const r = +Math.random() * 255
+    const g = +Math.random() * 255
+    const b = +Math.random() * 255
+    setInterval(getRandomColor, 1000)
+    return (`rgb(${r},${g},${b})`)
+}
+
+
+const getBoxes = () => {
+    for (let i = 0; i < 25; i++) {
+        const box = document.createElement('div')
+        wrapper.appendChild(box)
+        box.style.backgroundColor = getRandomColor()
+    }
+    
+}
+getBoxes();
+const getChangingColor = () => {
+    setInterval(() => {
+        const boxes = document.querySelectorAll('div');
+        for (let box of boxes) {
+            box.style.backgroundColor = getRandomColor()
+        }
+    }, 1000)
+}
+getChangingColor();
+
