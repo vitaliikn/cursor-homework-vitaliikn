2c06bf5cf740cf0b81b1844ed4d384b05cb5412b
vitaliikn <vitalja.dn@gmail.com>
2021-07-15 21:19:00 +0300

    add HW11

---
+++ HW11-promise/main.js
@@ -0,0 +1,18 @@
+async function getRandomChinese(l) {
+    let i = 0;
+    let symbolsArray = [];
+    while (i < l){
+      const randomChinese = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const symbol = Date.now().toString()%10e5;
+          const chinese = (String.fromCharCode(symbol));
+          resolve (chinese);
+          i++;
+          }, 50);        
+      });
+      symbolsArray.push(  await randomChinese);   
+    }
+    return symbolsArray.join(' ');
+  }
+  
+  getRandomChinese(4).then(console.log);
