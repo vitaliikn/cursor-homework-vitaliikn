ca0e535b6b96d6fb5828d69c8cf82e7287932b56
vitaliikn <vitalja.dn@gmail.com>
2021-07-23 17:05:56 +0300

    change HW10

--- HW10_events/main.js
+++ HW10_events/main.js
@@ -1,10 +1,24 @@
+const keys = document.querySelector('.wrapper')
+const buttons = document.querySelectorAll("div")
+const audio = document.querySelectorAll("audio")
 function playSound(e) {
     const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if (!sound) return;
     sound.currentTime = 0;
     sound.play();
 
-
 }
-
+keys.addEventListener('click', e => {
+    audio.forEach(s => {
+        if (s.className == e.target.id) {
+            buttons.forEach(b => {
+                if (b.id == e.target.id) {
+                    b.classList.add("sound")
+                }
+            })
+            s.play()
+        }
+        s.currentTime = 0
+    })
+})
 window.addEventListener('keydown', playSound);
